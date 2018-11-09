c50f80202830b73dc69e7f462d0568d217a93246
MdTabiSh <44886863+MdTabiSh@users.noreply.github.com>
2018-11-09 20:23:34 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+
+public class Hello{
+  public static void main(String args[]){
+  Scanner sc= new Scanner(System.in);
+  System.out.print("Enter your name: ");
+  String str= sc.nextLine();
+  System.out.println("Hello "+ str+"!");
+  System.out.println("Welcome to my coading World.");
+  }
